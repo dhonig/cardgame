48d2d9bab4b4b2e489648303ff1e6110148fb116
Daniel Honig <daniel.honig@gmail.com>
2020-02-27 02:57:28 -0500

    initial commit

---
+++ src/card-game-spec.js
@@ -0,0 +1,56 @@
+import {expect} from 'chai';
+import {CardDeck} from './index'
+import {Player} from './index'
+import {Game} from './index'
+
+describe('CardDeck', () => {
+    it('Is initialized with 52 cards', () => {
+        let aDeck = new CardDeck()
+        expect(aDeck.cards.length).to.eql(52);
+    });
+
+
+    it('It is in random order when it is shuffled.', () => {
+        let aDeck = new CardDeck();
+        aDeck.shuffle();
+        let anotherDeck = new CardDeck();
+        expect(aDeck).to.not.eql(anotherDeck);
+    });
+});
+
+
+describe('Player', () => {
+    it('Is initialized with an empty hand', () => {
+        let aPlayer = new Player();
+        expect(aPlayer.hand.length).to.eql(0);
+    })
+
+})
+
+
+describe('Game', () => {
+
+    it('Is initailized with the correct number of players', () => {
+        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 5});
+        expect(aGame.players.length).to.eql(5);
+    })
+    it('Is initialized with the correct number of cardsPerPlayer when passed an amount', () => {
+        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 7});
+        expect(aGame.cardsPerPlayer).to.eql(7);
+    })
+    it('Is initialized with the default number of cards when not passed the number of cards', () => {
+        let aGame = new Game({rounds: 4, numberOfPlayers: 5})
+        expect(aGame.cardsPerPlayer).to.eql(5);
+    })
+    it ('Deals the correct amount of cards to all players', () => {
+        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 7});
+        aGame.dealCardsToPlayers();
+        aGame.players.forEach(player=>{
+            expect(player.hand.length).to.eql(7)
+        })
+    })
+
+})
+
+
+
---
+++ src/index.js
@@ -0,0 +1,66 @@
+export class CardDeck {
+
+    static get Ranks() { return [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]; }
+    static get Suits(){ return ["Hearts", "Clubs", "Diamonds", "Spades"] }
+
+    constructor() {
+        this.cards = this.buildDeck();
+    }
+
+    buildDeck() {
+        let aDeck = new Array();
+        CardDeck.Suits.forEach((suit, index) => {
+            CardDeck.Ranks.forEach(rank => {
+                aDeck.push({suit: suit, rank: rank, weight: index + 2})
+            })
+        })
+        return aDeck;
+    }
+
+    shuffle() {
+        var currentIndex = this.cards.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = this.cards[currentIndex];
+            this.cards[currentIndex] = this.cards[randomIndex];
+            this.cards[randomIndex] = temporaryValue;
+        }
+        return this.cards;
+    }
+
+}
+
+export class Player {
+    constructor(){
+        this.hand=new Array();
+    }
+}
+
+export class Game {
+    constructor({rounds=5, numberOfPlayers=5, cardsPerPlayer=5}) {
+        this.rounds = rounds;
+        this.numberOfPlayers;
+        this.players = new Array();
+        this.deck = new CardDeck();
+        this.cardsPerPlayer= (cardsPerPlayer) ? cardsPerPlayer:5;
+        for(var i=0; i<numberOfPlayers; i++){
+            this.players.push(new Player());
+        }
+        this.deck.shuffle();
+    }
+    dealCardsToPlayers(){
+        for(let i=0; i < this.cardsPerPlayer; i++){
+            this.players.forEach( player=>{
+                player.hand.push(this.deck.cards.pop());
+            })
+        }
+    }
+
+}
+
