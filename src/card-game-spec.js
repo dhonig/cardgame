abff187b00fbdc85e995b60249e42ae56a3d50f8
Daniel Honig <daniel.honig@gmail.com>
2020-03-01 19:15:01 -0500

    finished with bonus features.

--- src/card-game-spec.js
+++ src/card-game-spec.js
@@ -1,9 +1,16 @@
 import {expect} from 'chai';
 import {CardDeck} from './index'
+import {Card} from './index'
 import {Player} from './index'
 import {Game} from './index'
 
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+
+
 describe('CardDeck', () => {
+
+
     it('Is initialized with 52 cards', () => {
         let aDeck = new CardDeck()
         expect(aDeck.cards.length).to.eql(52);
@@ -16,6 +23,8 @@ describe('CardDeck', () => {
         let anotherDeck = new CardDeck();
         expect(aDeck).to.not.eql(anotherDeck);
     });
+
+
 });
 
 
@@ -25,31 +34,120 @@ describe('Player', () => {
         expect(aPlayer.hand.length).to.eql(0);
     })
 
+
+    it('Correctly computes the sum of of a players hand', () => {
+        let card1 = new Card({rank: "2", suit: "Clubs", weight: 4})
+        let card2 = new Card({rank: "6", suit: "Clubs", weight: 8})
+        let card3 = new Card({rank: "J", suit: "Clubs", weight: 11})
+        let card4 = new Card({rank: "Q", suit: "Hearts", weight: 12})
+        let card5 = new Card({rank: "A", suit: "Spades", weight: 14})
+
+        let aPlayer = new Player()
+        aPlayer.hand = [card1, card2, card3, card4, card5]
+        expect(aPlayer.valueOfHand()).to.eql(49)
+
+    })
+
+
+    it('Can choose to trade in cards', () => {
+        let aPlayer = new Player()
+        expect(aPlayer.wantsToTrade()).to.be.a('boolean')
+    })
+
 })
 
 
 describe('Game', () => {
+
+
     it('Is initailized with the correct number of players', () => {
-        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 5});
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 5});
         expect(aGame.players.length).to.eql(5);
     })
-    it('Is initialized with the correct number of cardsPerPlayer when passed an amount', () => {
-        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 7});
+    it('Is initialized with the correct number of handSize when passed an amount', () => {
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 7});
         expect(aGame.cardsPerPlayer).to.eql(7);
     })
     it('Is initialized with the default number of cards when not passed the number of cards', () => {
-        let aGame = new Game({rounds: 4, numberOfPlayers: 5})
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5})
         expect(aGame.cardsPerPlayer).to.eql(5);
     })
-    it ('Deals the correct amount of cards to all players', () => {
-        let aGame = new Game({rounds: 4, numberOfPlayers: 5, cardsPerPlayer: 7});
+    it('Deals the correct amount of cards to all players', () => {
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 7});
         aGame.dealCardsToPlayers();
-        aGame.players.forEach(player=>{
+        aGame.players.forEach(player => {
             expect(player.hand.length).to.eql(7)
         })
-        let numberOfCardsDealt=aGame.numberOfPlayers*aGame.cardsPerPlayer;
-        expect(52-numberOfCardsDealt).to.eql(aGame.deck.cards.length);
+        let numberOfCardsDealt = aGame.numberOfPlayers * aGame.cardsPerPlayer;
+        expect(52 - numberOfCardsDealt).to.eql(aGame.deck.cards.length);
     })
+
+    it('Selects the top score from a list of score records', () => {
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 5});
+        let score1 = {score: 42, player: "Player 1"}
+        let score2 = {score: 20, player: "Player 1"}
+        let score3 = {score: 36, player: "Player 1"}
+        let scores = [score1, score2, score3]
+        let topScore = aGame.findWinningScore(scores);
+        expect(score1).to.eql(score1)
+    })
+
+    it('Correctly collects cards from players', () => {
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 5});
+        aGame.dealCardsToPlayers();
+        aGame.collectCardsFromPlayers();
+        aGame.players.forEach(player => expect(player.hand.length).to.eql(0))
+    })
+
+
+    it('Allows a player to trade/replace all but two cards', () => {
+        const personStub = sinon.stub(Player.prototype, "wantsToTrade").returns(true);
+        let person = new Player()
+        let person2 = new Player()
+        let aGame = new Game({rounds: 5, numberOfPlayers: 2, handSize: 5});
+        aGame.players = [person, person2]
+        aGame.dealCardsToPlayers()
+        let player1Hand = JSON.stringify(aGame.players[0])
+        let player2Hand = JSON.stringify(aGame.players[0])
+        aGame.manageTrades()
+        expect(player1Hand).to.not.equal(JSON.stringify(aGame.players[0].hand))
+        expect(player2Hand).to.not.equal(JSON.stringify(aGame.players[1].hand))
+
+        personStub.restore();
+    })
+
+    it('Simulates a game via the playGame method,recording the results of each round', () => {
+        let aGame = new Game({rounds: 4, numberOfPlayers: 5, handSize: 5});
+        aGame.playGame()
+        expect(aGame.recordOfRounds.length).to.eql(5)
+        aGame.recordOfRounds.forEach(round => {
+            expect(round.winner).to.not.be.null
+            expect(round.scores).to.not.be.null
+        })
+    })
+
+    it('Defaults to picking the best player by highest average', () => {
+        let aGame = new Game({rounds: 5, numberOfPlayers: 5, handSize: 5});
+        aGame.playGame()
+        const results = aGame.calculateBestPlayer();
+        expect(aGame.bestPlayerStrategy).to.eql(Game.BEST_PLAYER_BY_AVERAGE)
+        expect(results.message).to.eql(`By having the highest average score.`);
+
+    })
+
+    it('Can support different strategies for picking the best player', () => {
+        let aGame = new Game({
+            rounds: 100,
+            numberOfPlayers: 5,
+            handSize: 5,
+            bestPlayerStrategy: Game.BEST_PLAYER_BY_MOST_VICTORIES
+        });
+        aGame.playGame()
+        const results = aGame.calculateBestPlayer();
+        expect(aGame.bestPlayerStrategy).to.eql(Game.BEST_PLAYER_BY_MOST_VICTORIES)
+        expect(results.message).to.eql('By having the most victories.')
+    })
+
 
 })
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+export class Card {
+    constructor({suit, rank, weight}) {
+        this.suit = suit;
+        this.rank = rank;
+        this.weight = weight;
+    }
+}
+
+
 export class CardDeck {
 
-    static get Ranks() { return [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]; }
-    static get Suits(){ return ["Hearts", "Clubs", "Diamonds", "Spades"] }
+    static get Ranks() {
+        return [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+    }
+
+    static get Suits() {
+        return ["Hearts", "Clubs", "Diamonds", "Spades"]
+    }
+
+    static logCards(array) {
+        array.forEach(card => console.log(`Rank: ${rank}  Suit: ${suit} Weight: ${weight}`))
+    }
 
     constructor() {
         this.cards = this.buildDeck();
@@ -9,16 +27,16 @@ export class CardDeck {
 
     buildDeck() {
         let aDeck = new Array();
-        CardDeck.Suits.forEach((suit, index) => {
-            CardDeck.Ranks.forEach(rank => {
-                aDeck.push({suit: suit, rank: rank, weight: index + 2})
+        CardDeck.Suits.forEach((suit) => {
+            CardDeck.Ranks.forEach((rank, index) => {
+                aDeck.push(new Card({suit: suit, rank: rank, weight: index + 2}))
             })
         })
         return aDeck;
     }
 
     shuffle() {
-        var currentIndex = this.cards.length, temporaryValue, randomIndex;
+        let currentIndex = this.cards.length, temporaryValue, randomIndex;
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
             // Pick a remaining element...
@@ -36,30 +54,198 @@ export class CardDeck {
 }
 
 export class Player {
-    constructor(){
-        this.hand=new Array();
+    constructor() {
+        this.hand = new Array();
+        //A Player can trade in all but two cards once per round
+        this.hasTradedCards = false;
+    }
+
+
+    valueOfHand() {
+        //Use destructuring to compute weight
+        return this.hand.reduce((sum, {weight}) => sum + weight, 0);
+    }
+
+    //Randomly decide to trade in cards
+    // random thirty percent chance to trade
+    wantsToTrade() {
+        return (Math.floor(Math.random() * 101) > 70) ? true : false;
     }
 }
 
+
+export class BestPlayerByAverageScoreStrategy {
+
+
+    computeBestPlayer(aGame) {
+        let rounds = aGame.rounds;
+        let recordOfRounds = aGame.recordOfRounds;
+        let numberOfPlayer = aGame.numberOfPlayers;
+        let player = 'player1';
+        let averageScoresForPlayers = aGame.players.map((player, index) => {
+            let playerName = `player${index}`
+            return this.avgPerPlayer(playerName, rounds, recordOfRounds);
+        })
+        let winningAverage = this.findWinningAverage(averageScoresForPlayers);
+        return {player: winningAverage.player, message: "By having the highest average score."}
+    }
+
+    avgPerPlayer(player, rounds, recordOfRounds) {
+        let allRoundScores = recordOfRounds.map(element => element.scores)
+        let scores = [].concat.apply([], allRoundScores);
+        let scoresForPlayer = scores.filter(element => element.player === player)
+        let totalForPlayer = scoresForPlayer.reduce((sum, {score}) => sum + score, 0)
+        let average = Math.floor(totalForPlayer / rounds)
+        return {player: player, average: average}
+    }
+
+    findWinningAverage(scores) {
+        //Return the object with the max in score field
+        return scores.reduce((prev, current) => (prev.average > current.average) ? prev : current)
+    }
+
+
+}
+
+export class BestPlayerByMostVictoriesStrategy {
+    computeBestPlayer(aGame) {
+        let playerWithMostVictories = this.findPlayerWithMostVictories(aGame)
+        return {player: playerWithMostVictories.player, message: "By having the most victories."}
+    }
+
+    findPlayerWithMostVictories(aGame) {
+        //ToDo:  implement logic to handle ties and return array of tied players
+        let allRoundScores = aGame.recordOfRounds
+        let winners = aGame.recordOfRounds.map(element => element.winner)
+        let victoriesForPlayers = aGame.players.map((player, index) => {
+            let playerName = `player${index}`
+            let numberOfVictories = winners.filter(element => element.player === playerName)
+            return {player: playerName, total:numberOfVictories}
+        })
+
+        return this.findWinningestPlayer(victoriesForPlayers);
+    }
+
+    findWinningestPlayer(scores){
+        return scores.reduce((prev,current)=>(prev.total > current.total) ? prev: current )
+    }
+}
+
+
 export class Game {
-    constructor({rounds=5, numberOfPlayers=5, cardsPerPlayer=5}) {
+
+    //Best Player Strategies
+    static get BEST_PLAYER_BY_AVERAGE() {
+        return new BestPlayerByAverageScoreStrategy()
+
+    }
+
+    static get BEST_PLAYER_BY_MOST_VICTORIES() {
+        return new BestPlayerByMostVictoriesStrategy();
+    }
+
+    constructor({rounds = 5, numberOfPlayers = 5, handSize = 5, bestPlayerStrategy = Game.BEST_PLAYER_BY_AVERAGE}) {
         this.rounds = rounds;
-        this.numberOfPlayers=numberOfPlayers;
+        this.numberOfPlayers = numberOfPlayers;
         this.players = new Array();
         this.deck = new CardDeck();
-        this.cardsPerPlayer= (cardsPerPlayer) ? cardsPerPlayer:5;
-        for(var i=0; i<numberOfPlayers; i++){
+        //Change Strategy through constructor injection
+        this.bestPlayerStrategy = bestPlayerStrategy;
+        //Store the results of each round
+        this.recordOfRounds = new Array();
+        //If a number of cards is not specified default to 5
+        this.cardsPerPlayer = (handSize >= 0 && handSize != undefined) ? handSize : 5;
+        //Set up the game with the number of cards and players
+        //ToDo: throw an error if the max number of cards and players exceeds number of cards
+        for (var i = 0; i < this.numberOfPlayers; i++) {
             this.players.push(new Player());
         }
+        //Shuffle the deck as part of construction of the game
         this.deck.shuffle();
     }
-    dealCardsToPlayers(){
-        for(let i=0; i < this.cardsPerPlayer; i++){
-            this.players.forEach( player=>{
+
+    dealCardsToPlayers() {
+        for (let i = 0; i < this.cardsPerPlayer; i++) {
+            this.players.forEach(player => {
                 player.hand.push(this.deck.cards.pop());
             })
         }
     }
+
+
+    manageTrades() {
+        this.players.forEach((player, index) => {
+            //Players may elect to trade once per round
+            if (player.wantsToTrade() && player.hasTradedCards == false) {
+                console.log(`Player${index} would like to trade cards.`)
+                //Player may trade all but last two cards
+                let cardsTurnedIn = 0; //Record # of cards turned in so we can replace them.
+                while (player.hand.length > 2) {
+                    this.deck.cards.push(player.hand.pop())
+                    cardsTurnedIn += 1;
+                }
+                this.deck.shuffle();  //Ensure randomness of cards in deck
+                //Replace cardsTurnedIn
+                for (var i = 0; i < cardsTurnedIn; i++) {
+                    player.hand.push(this.deck.cards.pop())
+                }
+                //Mark player as having traded this round
+                player.hasTradedCards = true;
+            }
+        })
+    }
+
+    playGame() {
+        for (let i = 0; i <= this.rounds; i++) {
+            this.dealCardsToPlayers()
+            this.manageTrades();
+            let scores = this.sumPlayerScores()
+            let roundWinner = this.findWinningScore(scores)
+            this.recordOfRounds.push({winner: roundWinner, scores: scores})
+            this.collectCardsFromPlayers()
+            this.deck.shuffle(); //Set up for the next game
+            this.logRoundWinner(roundWinner, i + 1)
+            //Requirements were a little unclear, but we interpet as trading once a round.
+            //Removing this line would allow trading only once per game
+            this.players.forEach(player => this.playerHasTraded = false)
+        }
+
+        const results = this.calculateBestPlayer();
+        console.log(`Player: ${results.player}, is the best player, ${results.message}`);
+    }
+
+
+    findWinningScore(scores) {
+        //Return the object with the max in score field
+        return scores.reduce((max, scoreRecord) => (max.score > scoreRecord.score ? max : scoreRecord))
+    }
+
+    logRoundWinner(roundWinner, round) {
+        console.log(`Player: ${roundWinner.player} has won round ${round}, with a score of ${roundWinner.score}`)
+    }
+
+
+    sumPlayerScores() {
+        return this.players.map((player, index) => {
+            let playerName = `player${index}`
+            return {score: player.valueOfHand(), player: playerName}
+        })
+    }
+
+
+    collectCardsFromPlayers() {
+        this.players.forEach(player => {
+            while (player.hand.length != 0) {
+                this.deck.cards.push(player.hand.pop())
+            }
+        })
+
+    }
+
+    calculateBestPlayer() {
+        return this.bestPlayerStrategy.computeBestPlayer(this);
+    }
+
 
 }
 
